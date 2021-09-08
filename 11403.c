4894f2e1966293dc84a0568761f0498e95ee91e5
JHB98 <wjdanstn7806@naver.com>
2021-09-08 16:55:55 +0900

    Graph

---
+++ 11403.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdbool.h>
+bool a[100][100];
+int main()
+{
+    int n, ref;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &ref);
+            if (ref)
+            {
+                a[i][j] = true;
+            }
+        }
+    }
+    for (int k = 0; k < n; k++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (a[i][k] && a[k][j])
+                {
+                    a[i][j] = true;
+                }
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
